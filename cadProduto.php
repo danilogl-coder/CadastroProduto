fbc83fd72719e804f2ec5467c4cdbce233102318
danilo silva <daniloglcomercial@gmail.com>
2023-08-02 22:49:16 -0300

    .

---
+++ cadProduto.php
@@ -0,0 +1,20 @@
+<h1>Cadastro de Produto</h1>
+<form name="form1" action="" method="post" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label>Descrição</label>
+        <input type="text" name="descrição" class="form-control" value="">
+    </div>
+    <div class="mb-3">
+        <label>Valor de custo</label>
+        <input type="text" name="custo" class="form-control" value="">
+    </div>
+    <div class="mb-3">
+        <label>Fornecedor</label>
+        <input type="text" name="fornecedor" class="form-control" value="">
+    </div>
+    <div class="mb-3">
+        <label>Foto</label>
+        <input type="file" name="foto" class="form-control">
+    </div>
+    <input type="submit" class="btn btn-primary" value="Enviar">
+</form>
---
+++ principal.php
@@ -0,0 +1,77 @@
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+        <!-- Card Inicio 1 -->
+
+        <div class="card">
+        <img class="card-img-top" src="imagem/imagem.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        <!-- Card Fim 1 -->
+        </div>  
+        <div class="col">
+         <!-- Card Inicio 2 -->
+
+         <div class="card">
+        <img class="card-img-top" src="imagem/imagem.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        <!-- Card Fim 2 -->
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+         <!-- Card Inicio 3 -->
+
+         <div class="card">
+        <img class="card-img-top" src="imagem/imagem.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        <!-- Card Fim 3 -->
+        </div>
+        <div class="col">
+         <!-- Card Inicio 4 -->
+
+         <div class="card">
+        <img class="card-img-top" src="imagem/imagem.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        <!-- Card Fim 4 -->
+        </div>
+        <div class="col">
+         <!-- Card Inicio 5 -->
+
+         <div class="card">
+        <img class="card-img-top" src="imagem/imagem.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        <!-- Card Fim 5 -->
+        </div>
+    </div>
+</div>
